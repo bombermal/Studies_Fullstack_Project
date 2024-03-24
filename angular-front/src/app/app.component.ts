e483a9d40c5e59ca28d689de25e0e966bcc6fc84
Ivan Alisson Cavalcante Nunes de Lima <ivanalisson@hotmail.com>
2024-03-24 13:34:24 -0300

    New install Angular + Material

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './layout/top-bar/top-bar.component';
-import { FooterComponent } from './layout/footer/footer.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule
-    , RouterOutlet
-    , TopBarComponent
-    , FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Simplebank';
+  title = 'angular-front';
 }
