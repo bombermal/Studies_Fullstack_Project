4cbdac3274ac9490affa7e155eb47f7098b7b8ae
Ivan Alisson Cavalcante Nunes de Lima <ivanalisson@hotmail.com>
2024-03-23 21:41:39 -0300

    Stage: Test layout

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule
+    , RouterOutlet
+    , TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-front';
+  title = 'Simplebank';
 }
