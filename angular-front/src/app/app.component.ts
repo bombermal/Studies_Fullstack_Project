c6830face1030246ca8464140a33b6555ac81a19
Ivan Alisson Cavalcante Nunes de Lima <ivanalisson@hotmail.com>
2024-03-24 19:38:57 -0300

    Stage sidenav replace

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DomSanitizer } from '@angular/platform-browser';
 
 // Components
 import { HeaderComponent } from './header/header.component';
