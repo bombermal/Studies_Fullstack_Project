b9b02ee77255a89b569c87f8411f2073eda32ba0
Ivan Alisson Cavalcante Nunes de Lima <ivanalisson@hotmail.com>
2024-03-24 23:47:45 -0300

    Config ng2-charts

--- angular-front/src/app/dash/card/card.component.ts
+++ angular-front/src/app/dash/card/card.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input  } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-  @Input() title: string;
+  // @Input() title: string;
 }
